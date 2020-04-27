36b133a1f00310b546f78109eaa062bc3adc84ba
hsinpa <hsinpa@gmail.com>
2020-04-27 18:05:13 +0800

    Add mesh (poor performance maybe)

---
+++ Assets/Script/GPUInstanceMeshScript.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GPUInstanceMeshScript : MonoBehaviour
+{
+    [SerializeField]
+    Texture mainTexture;
+
+    [SerializeField]
+    Texture depthTexture;
+
+    [SerializeField]
+    ComputeShader computeShader;
+
+    [SerializeField]
+    Material material;
+
+    private Vector3 objectPosition;
+    public float range;
+    private Bounds bounds;
+
+    public RenderTexture mainRenderTexture;
+    public RenderTexture depthRenderTexture;
+
+    [SerializeField]
+    bool isUpdate;
+
+    [SerializeField]
+    private MeshFilter meshFilter;
+
+    private Mesh mesh;
+
+    public Vector2 size;
+
+    Vector3[] positions;
+    int[] indicies;
+    int[] triangles;
+    Vector3[] normals;
+    Color[] colors;
+
+    private int textureSize = 256;
+    int bufferSize => textureSize * textureSize;
+
+    ComputeBuffer _positionBuffer;
+    ComputeBuffer _normalBuffer;
+    ComputeBuffer _colorBuffer;
+    ComputeBuffer _indicesBuffer;
+
+    private void Start()
+    {
+        //Vector3
+        //_positionBuffer = new ComputeBuffer(textureSize * textureSize, floatSize * 3);
+        //_normalBuffer = new ComputeBuffer(textureSize * textureSize, floatSize * 3);
+        //_colorBuffer = new ComputeBuffer(textureSize * textureSize, floatSize * 4);
+        //_indicesBuffer = new ComputeBuffer(textureSize * textureSize, floatSize);
+    }
+
+    private void InitMesh()
+    {
+        mesh = new Mesh();
+        mesh.SetVertices(new Vector3[1]);
+        mesh.SetIndices(new[] { 0 }, MeshTopology.Points, 0);
+
+        meshFilter.mesh = mesh;
+        mesh.UploadMeshData(true);
+    }
+
+    private RenderTexture CreateRTexture(int width, int height, Texture rawTexture) {
+        RenderTexture mainRenderTexture = new RenderTexture(width, height, 8);
+        mainRenderTexture.enableRandomWrite = true;
+        mainRenderTexture.Create();
+
+        Graphics.Blit(rawTexture, mainRenderTexture);
+
+        return mainRenderTexture;
+    }
+
+    private ComputeBuffer SetComputeBuffer<T>(ComputeBuffer computeBuffer, T[] emptyArray, int size, ComputeBufferType bufferType = ComputeBufferType.Default) {
+        computeBuffer = new ComputeBuffer(bufferSize, size, bufferType);
+        computeBuffer.SetData(emptyArray);
+        return computeBuffer;
+    }
+
+    void InitData()
+    {
+        bounds = new Bounds(transform.position, Vector3.one * (range + 1));
+
+        positions = new Vector3[bufferSize];
+        colors = new Color[bufferSize];
+        indicies = new int[bufferSize];
+        normals = new Vector3[bufferSize];
+    }
+
+    void Update()
+    {
+
+        objectPosition = transform.localPosition;
+        if (mainTexture == null || depthTexture == null)
+            return;
+
+        //if (positions == null || mesh == null)
+
+        if (isUpdate)
+        {
+            InitData();
+
+            int kernelHandle = computeShader.FindKernel("GetMesh");
+
+            computeShader.SetInt("TexWidth", textureSize);
+            int floatSize = sizeof(float);
+
+            _positionBuffer = SetComputeBuffer(_positionBuffer, positions, floatSize * 3, ComputeBufferType.Default);
+            _normalBuffer = SetComputeBuffer(_normalBuffer, normals, floatSize * 3, ComputeBufferType.Default);
+            _colorBuffer = SetComputeBuffer(_colorBuffer, colors, floatSize * 4, ComputeBufferType.Default);
+            _indicesBuffer = SetComputeBuffer(_indicesBuffer, indicies, floatSize, ComputeBufferType.Default);
+
+
+            mainRenderTexture = CreateRTexture(textureSize, textureSize, mainTexture);
+            depthRenderTexture = CreateRTexture(textureSize, textureSize, depthTexture);
+
+            computeShader.SetTexture(kernelHandle, "MainTex", mainRenderTexture);
+            computeShader.SetTexture(kernelHandle, "DepthTex", depthRenderTexture);
+
+            computeShader.SetBuffer(kernelHandle, "PositionBuffer", _positionBuffer);
+            computeShader.SetBuffer(kernelHandle, "ColorBuffer", _colorBuffer);
+            computeShader.SetBuffer(kernelHandle, "IndicesBuffer", _indicesBuffer);
+            computeShader.SetBuffer(kernelHandle, "NormalBuffer", _normalBuffer);
+
+            //    Debug.Log(textureSize / 16);
+            computeShader.Dispatch(kernelHandle, textureSize / 16, textureSize / 16, 1);
+
+
+            //Graphics.DrawMeshInstancedIndirect(mesh, 0, material, bounds, argsBuffer);
+
+            isUpdate = false;
+        }
+    }
+
+    private void OnRenderObject()
+    {
+        material.SetPass(0);
+
+        material.SetVector("_ObjectPosition", objectPosition);
+
+        material.SetBuffer("_PositionBuffer", _positionBuffer);
+
+        material.SetBuffer("_ColorBuffer", _colorBuffer);
+
+        material.SetFloat("_Width", textureSize);
+
+        material.SetFloat("_TotalVertex", bufferSize);
+
+        Graphics.DrawProceduralNow(MeshTopology.Points, bufferSize, 1);
+
+        //ResetData();
+    }
+
+
+    void ResetData() {
+        _positionBuffer.Dispose();
+        _colorBuffer.Dispose();
+        _indicesBuffer.Dispose();
+        _normalBuffer.Dispose();
+    }
+}
---
+++ Assets/Shader/PointMeshShader.shader
@@ -0,0 +1,109 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Hsinpa/PointMeshShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _PointSize ("Point Size", Range(0.01, 0.1)) = 1
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+        Cull off
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma geometry geom
+            #pragma target 5.0
+
+            #include "UnityCG.cginc"
+
+            struct v2g {
+                float4 vertex : SV_POSITION;
+                //float2 uv : TEXCOORD0;
+                //float3 normal : NORMAL;
+                fixed4 color : COLOR;
+                uint id : NUMBER;
+            };
+
+            struct g2f {
+                float4 worldPos : SV_POSITION;
+                fixed4 color : COLOR;
+                //float2 uv : TEXCOORD0;
+                //fixed4 col : COLOR;
+            };
+
+            uniform float3 _ObjectPosition;
+            uniform int _Width;
+            uniform int _TotalVertex;
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            float _PointSize;
+
+            StructuredBuffer<float3> _PositionBuffer;
+            StructuredBuffer<float4> _ColorBuffer;
+            StructuredBuffer<int> _Triangle;
+
+            v2g vert (uint id : SV_VertexID)
+            {
+                v2g o;
+
+                float3 objectPosition = _ObjectPosition + _PositionBuffer[id];
+
+                o.vertex = fixed4((objectPosition),0);
+                o.color = _ColorBuffer[id];
+                o.id = id;
+
+                return o;
+            }
+
+            [maxvertexcount(4)]
+            void geom(point v2g IN[1], inout TriangleStream<g2f> triStream) {
+                int id = IN[0].id;
+                if (IN[0].id + _Width > _TotalVertex) return;
+
+                g2f o;
+
+                float3 topAngle = float3(0, 1, 0);
+                float3 rightAngle = float3(1, 0, 0);
+                o.color = IN[0].color;
+
+                float3 basePos1 = IN[0].vertex.xyz;
+                float3 basePos2 = _ObjectPosition + _PositionBuffer[id+1];
+                float3 basePos3 = _ObjectPosition + _PositionBuffer[id + _Width];
+                float3 basePos4 = _ObjectPosition + _PositionBuffer[id + _Width +1];
+
+                //o.worldPos = UnityObjectToClipPos(basePos1 - rightAngle * _PointSize * 0.5);
+                o.worldPos = UnityObjectToClipPos(basePos1);
+                triStream.Append(o);
+
+                o.worldPos = UnityObjectToClipPos(basePos2);
+                triStream.Append(o);
+
+                o.worldPos = UnityObjectToClipPos(basePos3);
+                triStream.Append(o);
+
+                o.worldPos = UnityObjectToClipPos(basePos4);
+                triStream.Append(o);
+
+                triStream.RestartStrip();
+            }
+
+            fixed4 frag (g2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = i.color;
+
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
